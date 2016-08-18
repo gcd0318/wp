dd3a4fc2532c6c42c27f69670f8446c6c4bdd6dd
gcd0318 <gcd0318@hotmail.com>
2016-08-19 02:19:55 +0800

    separate fetch and get_page

--- wp.py
+++ wp.py
@@ -23,6 +23,8 @@ class WPHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if(('h3' == tag.lower()) and (('class', 'storytitle') in attrs)):
             self.pn = self.pn + 1
+            f = open(self.fp + '/' + str(self.pn) + '.txt', 'w')
+            f.close()
         if('a' == tag.lower()):
             if((('rel', 'bookmark') in attrs) or (('rel', 'category tag') in attrs)):
                 self.starttags.append(tag.lower())
@@ -44,11 +46,9 @@ class WPHTMLParser(HTMLParser):
             if(0 < len(data)):
                 if(0 < len(self.starttags)):
                     if('a' == self.starttags[-1]):
-                        print(data)
-                        print(data, file=f)
+                        f.write(data + '\n')
                     if('div' == self.starttags[-1]):
-                        print(data)
-                        print(data, file=f)
+                        f.write(data + '\n')
             f.close()
 
 
@@ -59,65 +59,73 @@ def gen_datestr(dtstr, delta=0, fmt='%Y/%m/%d'):
     nt = time.strptime(dtstr, fmt)
     return (datetime.date(nt[0],nt[1],nt[2])+datetime.timedelta(delta)).strftime(fmt)
 
+def get_page(root, datestr):
+    import urllib.request, urllib.error
+    post_num = -1
+    e = 'NULL'
+    try:
+        page = urllib.request.urlopen(root+gen_datestr(datestr)).read().decode('utf8')
+        fn = datestr+'/page.html'
+        if(not os.path.exists(datestr)):
+            os.makedirs(datestr)
+        f = open(fn, 'w', encoding='utf8')
+        f.write(page)
+        f.close()
+    except Exception as err:
+        e = str(err)
+        if(urllib.error.HTTPError == type(err))and(404 == err.code):
+            post_num = 0
+            e = '404'
+    return post_num, e
 
 def fetch(params):
-    import urllib.request, urllib.error
-    root = params['root']
-    start_date = params['start_date']
+    start_date = params['post']['start_date']
     delta = 0
     while(True):
         sqls = []
-        post_num = -1
         datestr = gen_datestr(start_date, delta)
-        try:
-            sqls.append("insert into post_record (post_date, post_num) values('"+datestr+"', " + str(post_num) + ")")
-            page = urllib.request.urlopen(root+gen_datestr(datestr)).read().decode('utf8')
-            fn = datestr+'/page.html'
-            if(not os.path.exists(datestr)):
-                os.makedirs(datestr)
-            f = open(fn, 'w', encoding='utf8')
-            f.write(page)
-            f.close()
-#            sqls.append("insert into post_record (post_date, post_num) values('"+datestr+"', " + str(post_num) + ")")
-        except Exception as err:
-            e = str(err)
-            if(urllib.error.HTTPError == type(err))and(404 == err.code):
-                post_num = 0
-                e = '404'
-            sqls.append("update post_record set status = '" + e + "', post_num =" + str(post_num) + " where post_date = '" + datestr + "';")
-        finally:
-            delta = delta + 1
-            for sql in sqls:
-                print(sql)
-                db(params, sql)
-            time.sleep(5)
+        sqls.append("insert into post_record (post_date, post_num) values('"+datestr+"', -1)")
+        post_num, e = get_page(params['post']['root'], datestr)
+        sqls.append("update post_record set status = '" + e + "', post_num =" + str(post_num) + " where post_date = '" + datestr + "';")
+        delta = delta + 1
+        for sql in sqls:
+            db(params['db'], sql)
+        time.sleep(5)
     
 
 def gen_txt(params):
 # https://docs.python.org/3/library/html.parser.html
     while(True):
-        posts = db(params, 'select post_date from post_record where post_num = -1')
-        for ps in posts:
-            post_date = ps[0]
-            f = open(post_date+'/page.html', 'r')
-#            ls = f.readlines()
-            page = f.read()
-            f.close()
-            wphp = WPHTMLParser(post_date)
-            wphp.feed(cutstr(page, params['head'], params['tail']))
-            print('=============================')
-            wphp.close()
+        posts = db(params['db'], 'select post_date, status from post_record where post_num = -1')
+        if(0 == len(posts)):
+            time.sleep(60)
+        else:
+            for ps in posts:
+                post_date = ps[0]
+                status = ps[1]
+                if(status):
+                    post_num, e = get_page(params['post']['root'], post_date)
+                    db(params['db'], "update post_record set status = '" + e + "', post_num =" + str(post_num) + " where post_date = '" + post_date + "';")
+                f = open(post_date+'/page.html', 'r')
+                page = f.read()
+                f.close()
+                wphp = WPHTMLParser(post_date)
+                wphp.feed(cutstr(page, params['page']['head'], params['page']['tail']))
+                wphp.close()
+                n = len(page.split(params['page']['title']))
+                db(params['db'], "update post_record set post_num = " + str(n-1) + " where post_date = '" + post_date + "';")
 
 
 def parse_txt(txt):
     tmp = txt
 
-def db(params, sql):
+def db(dbp, sql):
+    print(sql)
     import mysql.connector
     from mysql.connector import connection
     sql = sql.strip()
     res = None
-    cnx = connection.MySQLConnection(user=params['db']['dbuser'], password=params['db']['dbpassword'],host=params['db']['dbhost'],database=params['db']['database'])
+    cnx = connection.MySQLConnection(user=dbp['dbuser'], password=dbp['dbpassword'],host=dbp['dbhost'],database=dbp['database'])
     c = cnx.cursor()
     c.execute(sql)
     if(sql.startswith('select')):
@@ -128,13 +136,15 @@ def db(params, sql):
 
 def init():
     start_date = '2005/08/28'
-    params = {'start_date': '',\
-              'root': 'https://gcd0318.wordpress.com/',\
-              'head': '<!-- end header -->',\
-              'tail':'<!-- begin footer -->',\
-              'title': '<h3 class="storytitle">',\
-              'meta': '<div class="meta">',\
-              'content':'<div class="storycontent">',\
+    params = {'post':{'start_date': '',\
+                      'root': 'https://gcd0318.wordpress.com/',\
+                    },\
+              'page':{'head': '<!-- end header -->',\
+                      'tail':'<!-- begin footer -->',\
+                      'title': '<h3 class="storytitle">',\
+                      'meta': '<div class="meta">',\
+                      'content':'<div class="storycontent">',\
+                    },\
               'db':{'dbhost':'192.168.1.18',\
                     'dbuser':'wp',\
                     'dbpassword':'wp',\
@@ -144,11 +154,11 @@ def init():
                     'database':'wp',\
                 },\
               }
-    sd = db(params, 'select post_date from post_record order by post_date desc limit 1')
+    sd = db(params['db'], 'select post_date from post_record order by post_date desc limit 1')
     if(0 < len(sd)):
-        params['start_date'] = sd[0][0]
+        params['post']['start_date'] = sd[0][0]
     else:
-        params['start_date'] = start_date
+        params['post']['start_date'] = start_date
     return params
 
 
